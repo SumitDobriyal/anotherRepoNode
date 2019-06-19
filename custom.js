55d84cfcf4288f5113ee15919611b582492da334
Sumit <dobriyal.sumit@gmail.com>
2019-06-19 10:25:07 +0530

    Another node project commit

---
+++ custom.js
@@ -0,0 +1,11 @@
+const main = {};
+main.subtract = (num1, num2) => {
+	return num1 - num2;
+}
+main.multi = (num1,num2) => {
+	return num1 * num2;
+}
+
+//module.exports = subtract;
+//module.exports = multi;
+module.exports = main;
---
+++ index.js
@@ -0,0 +1,3 @@
+const minus = require('./custom');
+
+console.log("this is minus function " + minus.subtract(10,5));
